e5772f97b20ab71c6b6442f10ce4f0bbcc0ef2a4
ErenHatirnaz <erenhatirnaz@hotmail.com.tr>
2015-06-23 01:27:29 +0300

    Added 2 more new tasks: zip and clean.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 
 var jade = require('gulp-jade'),
 	sass = require('gulp-sass'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	zip = require('gulp-zip'),
+	del = require('del');
 
 gulp.task('jade', function(){
 	gulp.src('src/*.jade')
@@ -20,6 +22,17 @@ gulp.task('coffee', function(){
 	gulp.src('src/coffee/*.coffee')
 		.pipe(coffee())
 		.pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('zip', ['jade', 'sass', 'coffee'], function(){
+	gulp.src("dist/*")
+		.pipe(zip('hellogulpjs.zip'))
+		.pipe(gulp.dest(""))
+});
+
+gulp.task('clean', function(){
+	del("dist/");
+	del("hellogulpjs.zip")
 });
 
 gulp.task('default', ['jade', 'sass', 'coffee']);
